99a9db4edcf5315f521b1eeed6b26204f15e235e
wangjunbo <1257680563@qq.com>
2025-05-08 16:30:30 +0800

    fix21

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,43 @@
-package com.example.helloworld
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        setupWebView()
+        webView.loadUrl("https://t.ddz.cool/?room=wang1991")
+    }
+
+    private fun setupWebView() {
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            allowFileAccess = true
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url ?: "")
+                return true
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        webView.clearHistory()
+        webView.clearCache(true)
+        webView.loadUrl("about:blank")
+        webView.removeAllViews()
+        webView.destroy()
+        super.onDestroy()
     }
 }
