64086fa75f0d7b0af5f493f1e1172da1b182d2a9
wangjunbo <1257680563@qq.com>
2025-05-08 16:07:02 +0800

    修改主页逻辑

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,17 @@
-package com.example.helloworld
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient() // 确保网页在WebView中打开
+        webView.loadUrl("https://t.ddz.cool/?room=wang1991")
     }
 }
