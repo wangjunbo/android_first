99f560bbe5ca284cab8970ad6b742815a711d2d1
wangjunbo <1257680563@qq.com>
2025-05-08 16:16:19 +0800

    回滚了

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,17 +1,11 @@
-import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+package com.example.helloworld
+
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val webView: WebView = findViewById(R.id.webview)
-        webView.settings.javaScriptEnabled = true
-        webView.webViewClient = WebViewClient() // 确保网页在WebView中打开
-        webView.loadUrl("https://t.ddz.cool/?room=wang1991")
     }
 }
